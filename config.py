6b8135b9c7bd764f99607099c5be6115d6e78aec
Pranav Barathwaj Padmanaban <pranavbp1810@gmail.com>
2025-03-13 12:47:44 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
 COMPANIES = ['sps', 'cascade', 'shopify', 'pel']
-REPRESENTATIVES = ['Adam', 'Beth', 'Bret', 'Brian', 'Butch', 'Courtney', 'Isabelle', 'Jim', 'John', 'Jon', 'Kyle', 'Zach']
+REPRESENTATIVES = ['Adam', 'Beth', 'Bret', 'Brian', 'Butch', 'Courtney', 'Isabelle', 'Jim', 'John', 'Jon', 'Kyle', 'Zach', 'Huber', 'Steadfast']
